ac66c4cd851be8442ddd3370b6396f99a2db2af5
Olivier Lalonde <olalonde@gmail.com>
2011-05-14 18:33:11 -0400

    initial commit, business and search work

---
+++ demo/yelp.js
@@ -0,0 +1,18 @@
+var yelp = require("./yelp/index").createClient({
+  consumer_key: "consumer-key", 
+  consumer_secret: "consumer-secret",
+  token: "token",
+  token_secret: "token-secret"
+});
+
+
+yelp.search({term: "food", location: "Montreal"}, function(error, data) {
+  console.log(error);
+  console.log(data);
+});
+
+yelp.business("yelp-san-francisco", function(error, data) {
+  console.log(error);
+  console.log(data);
+});
+
