928f6114905d20db7684edae28b07c04fdde673c
Olivier Lalonde <olalonde@gmail.com>
2014-02-28 07:38:46 +0800

    demo can be run using env variables

--- demo/yelp.js
+++ demo/yelp.js
@@ -1,8 +1,9 @@
 var yelp = require("../index").createClient({
-  consumer_key: "consumer-key", 
-  consumer_secret: "consumer-secret",
-  token: "token",
-  token_secret: "token-secret"
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  token: process.env.TOKEN,
+  token_secret: process.env.TOKEN_SECRET,
+  ssl: true
 });
 
 
